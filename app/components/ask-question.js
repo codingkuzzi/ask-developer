ba638771e90a047a81b24239672e404d5ad6542c
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-08-11 17:42:29 -0700

    add questions form

---
+++ app/components/ask-question.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    createQuestion() {
+      var date = new Date();
+      var dMonth = date.getMonth()+1;
+      var dateString = dMonth+'/'+date.getDate()+'/'+date.getFullYear();
+      var params = {
+        author: this.get('author'),
+        title: this.get('inputTitle'),
+        question: this.get('inputQuestion'),
+        image: this.controller.get('inputImageUrl'),
+        timestamp: dateString
+      };
+    //  console.log(this);
+      this.sendAction('createQuestion', params);
+      var newQuestion = this.store.createRecord('question', params);
+            newQuestion.save();
+            this.transitionTo('index', {reload: true});
+    } //end of createQuestion action
+  } //end of actions
+});
--- app/routes/ask-question.js
+++ app/routes/ask-question.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+    //  console.log(this);
+      //this.sendAction('createQuestion', params);
+        newQuestion.save();
+        this.transitionTo('index', {reload: true});
+    } //end of createQuestion action
+  } //end of actions
 });
